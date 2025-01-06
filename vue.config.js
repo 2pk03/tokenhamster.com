b4a9d7f57c25f236ff1aec70dee017cc4cd6db01
2pk03 <alo.alt@me.com>
2025-01-06 19:14:36 +0100

    FE

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,4 @@
-const { defineConfig } = require('@vue/cli-service');
-
+const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  chainWebpack: (config) => {
-    config.module
-      .rule('css') // Target CSS files
-      .use('css-loader') // Use the css-loader
-      .loader('css-loader')
-      .tap((options) => {
-        options.url = false; // Disable URL processing
-        return options;
-      });
-  },
-});
-
+  transpileDependencies: true
+})
