ae982248d1475d186ce488c20781b62779b85b4f
2pk03 <alo.alt@me.com>
2025-01-12 19:28:44 +0100

    candlestick

--- src/components/MonthChart.vue
+++ src/components/MonthChart.vue
@@ -349,13 +349,13 @@ export default {
 
         formatVolume(value) {
             if (value >= 1e9) {
-                return `${(value / 1e9).toFixed(2)}B`; // Format as billions
+                return `${(value / 1e9).toFixed(2)}B`; 
             } else if (value >= 1e6) {
-                return `${(value / 1e6).toFixed(2)}M`; // Format as millions
+                return `${(value / 1e6).toFixed(2)}M`; 
             } else if (value >= 1e3) {
-                return `${(value / 1e3).toFixed(1)}K`; // Format as thousands
+                return `${(value / 1e3).toFixed(1)}K`; 
             }
-            return value.toLocaleString(); // Add thousand separators
+            return value.toLocaleString(); 
         },
 
         updateCandlestickChart(data) {
